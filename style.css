c9c62a96977deb53d4c225321f6b7421523d15c3
kmj19086-bit <kmj19086@gmail.com>
2025-12-06 18:55:30 +0900

    Update style.css

--- style.css
+++ style.css
@@ -1,1 +1,21 @@
+/* style.css 파일 */
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    display: flex; 
+}
 
+#sidebar {
+    width: 300px;
+    padding: 20px;
+    height: 100vh;
+    overflow-y: auto;
+    border-right: 1px solid #ccc;
+    background-color: #f8f8f8;
+}
+
+#map {
+    flex-grow: 1; 
+    height: 100vh;
+}
