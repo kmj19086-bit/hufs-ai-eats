82749bf80cfcd4589b265ff842456cf0ad9e3bb6
kmj19086-bit <kmj19086@gmail.com>
2025-12-06 18:55:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
-/* style.css 파일 */
-body {
-    font-family: Arial, sans-serif;
-    margin: 0;
-    padding: 0;
-    display: flex; 
-}
+/* script.js 파일 */
+// 1. 지도를 담을 HTML 요소
+var mapContainer = document.getElementById('map'); 
+var mapOption = {
+    // 외대 중심 좌표 (임시 좌표)
+    center: new kakao.maps.LatLng(37.5957, 127.0592), 
+    level: 3 
+};
 
-#sidebar {
-    width: 300px;
-    padding: 20px;
-    height: 100vh;
-    overflow-y: auto;
-    border-right: 1px solid #ccc;
-    background-color: #f8f8f8;
-}
-
-#map {
-    flex-grow: 1; 
-    height: 100vh;
-}
+// 2. 지도를 생성합니다.
+var map = new kakao.maps.Map(mapContainer, mapOption);
